0dcf73c9e3a38201719957aad57d60e8ada73a94
Ted Kirkpatrick <ted@sfu.ca>
2013-04-07 20:51:24 -0700

    WIP

--- typed-iswim-ch15.rkt
+++ typed-iswim-ch15.rkt
@@ -11,13 +11,14 @@
   (reduction-relation
    t-iswim
    (--> (in-hole E ((λ X T M) V))
-        (in-hole E (t-subst V X M))
+        (in-hole E (t-subst M X V))
         βv)
    (--> (in-hole E (o b ...))
         (in-hole E (δ (o b ...)))
         δ)))
 
 (define-metafunction t-iswim
+  t-subst : any X any -> any
   ;; 1. X_1 bound, so don't continue in λ body
   [(t-subst (λ X_1 T any_1) X_1 any_2)
    (λ X_1 T any_1)] 
@@ -37,17 +38,20 @@
   [(t-subst any_2 X_1 any_1) any_2])
 
 (define-metafunction t-iswim
+  t-subst-var : any X Y -> any
   [(t-subst-var (any_1 ...) variable_1 variable_2)
    ((t-subst-var any_1 variable_1 variable_2) ...)]
   [(t-subst-var variable_1 variable_1 variable_2) variable_2]
   [(t-subst-var any_1 variable_1 variable_2) any_1])
 
 (define-metafunction t-iswim
+  Β : any -> num or #f
   [(Β number) num]
   [(Β any) #f]
   )
 
 (define-metafunction t-iswim
+  Δ : any -> num or (-> num (-> num num)) or #f
   [(Δ (iszero num)) (-> num (-> num num))]
   [(Δ (add1 num)) num]
   [(Δ (sub1 num)) num]
@@ -60,6 +64,7 @@
   )
 
 (define-metafunction t-iswim
+  TC : ((X T) ...) M -> T or #f
   [(TC Γ b)
    (Β b)]
   [(TC Γ X)
@@ -72,19 +77,63 @@
    (TCo (o (TC Γ M) ...))])
 
 (define-metafunction t-iswim
+  TCvar : ((X T) ...) X -> T or #f
   [(TCvar ((X T_1) ... (Y T_2) (Z T_3) ...) Y)
    T_2
    (side-condition (not (member (term Y) (term (Z ...)))))]
   [(TCvar Γ X) #f])
 
 (define-metafunction t-iswim
+  TCλ : T any -> (-> T S) or #f
   [(TCλ T S) (-> T S)]
   [(TCλ T #f) #f])
 
 (define-metafunction t-iswim
+  TCapp : any any -> T or #f
   [(TCapp (-> S T) S) T]
   [(TCapp any_1 any_2) #f])
 
 (define-metafunction t-iswim
+  TCo : (o any ...) -> T or #f
   [(TCo (o T ...)) (Δ (o T ...))]
   [(TCo (o any ...)) #f])
+
+(define testTC
+  (lambda ()
+    (test-equal
+     (term (TC () ((λ x (-> num num) x) 5)))
+     (term #f))
+    (test-equal
+     (term (TC () (+ x y)))
+     (term #f))
+    (test-equal
+     (term (TC () (+ 1 2)))
+     (term num))
+    (test-equal
+     (term (TC () (((λ x num (λ y num x)) 2) 3)))
+     (term num))
+    (test-equal
+     (term (TC () ((λ x num 2) 4)))
+     (term num))
+    (test-equal
+     (term (TC () (+ ((λ x num x) 1) 2)))
+     (term num))
+    
+    (test-results)))
+
+(define testProg
+  (λ ()
+    (test-->> t-iswim-red
+              (term ((λ x num 2) 4)) 2)
+    (test-->> t-iswim-red
+              (term (((λ x num (λ y num x)) 2) 3)) 2)
+    (test-->> t-iswim-red
+              (term (+ ((λ x num x) 1) 2)) 3)
+    
+    (test-results)))
+
+(define all-tests
+  (λ ()
+    (testTC)
+    (testProg)))
+                   
