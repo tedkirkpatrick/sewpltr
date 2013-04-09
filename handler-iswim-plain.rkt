b0b305363fcd2cf93d9cccfba416370f559a47b2
Ted Kirkpatrick <ted@sfu.ca>
2013-04-09 08:19:07 -0700

    Working on handler (Chapter 8.3). File handler-iswim-plain created for simpler language

---
+++ handler-iswim-plain.rkt
@@ -0,0 +1,35 @@
+#lang racket
+(require "compiled/iswim_rkt.zo")
+
+(define-extended-language handler-iswim
+  iswim
+  (M .... (throw b) (catch M_1 with (λ X M_2)))
+  (E hole (V E) (E M) (o V ... E M ... ))
+  )
+
+(define-metafunction/extension subst iswim
+  [(subst-handler (catch M_1 with (λ X_1 M_2)) X_2 M_3)
+   (catch (subst-err M_1 X_2 M_3) with (subst (λ X_1 M_2) X_2 M_3))]
+  )
+
+(define handler-red
+  (extend-reduction-relation
+   iswim-red
+   handler-iswim
+   ; Override βv rule in iswim
+   (--> (in-hole E ((λ X M) V))
+        (in-hole E (subst-handler (M X V)))
+        βb)
+   (--> (in-hole E (catch (throw b) with (λ X M)))
+        (in-hole E ((λ X M) b))
+        catch)
+   (--> (in-hole E (throw b))
+        (throw b)
+        throw)
+   (--> (in-hole E (catch V with (λ X M)))
+        (in-hole E V)
+        return)
+   ))
+   
+
+(current-traced-metafunctions 'all)
