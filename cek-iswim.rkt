7fe6f478f73d00c09d62c2f2c0710d8565a65793
Ted Kirkpatrick <ted@sfu.ca>
2013-04-15 13:35:32 -0700

    SEK reduction for ISWIM passes basic tests

--- cek-iswim.rkt
+++ cek-iswim.rkt
@@ -5,18 +5,21 @@
 
 (define-extended-language cek-iswim
   iswim
-  (κ mt (fn (V ε) κ) (ar (M ε) κ) (op ((V ε) ... o) ((M ε) ...) κ))
-  (ε ((X (M ε)) ...))
+  (Cl (M ε))  ; Closure
+  (Val (V ε)) ; Closure of a value. In ISWIM, is ε guaranteed to be null?
+  (κ mt (fn Cl κ) (ar Cl κ) (op (Val ... o) (Cl ...) κ))
+  (Ev (X Cl))
+  (ε (Ev ...))
   )
 
 (define-metafunction cek-iswim
-  [(update ((U ε_u) ...) X (V ε_v)) ((V ε_v) (U ε_u) ...)]
+  [(update (Ev ...) X Val) ((X Val) Ev ...)]
   )
 
 (define-metafunction cek-iswim
   [(lookup X ()) (0 ())]
-  [(lookup X ((X (V ε_v)) (U ε_u) ...)) (V ε_v)]
-  [(lookup X ((Y (V ε_v)) (U ε_u) ...)) (lookup X ((U ε_u) ...))]
+  [(lookup X ((X Val) Ev ...)) Val]
+  [(lookup X ((Y Val) Ev ...)) (lookup X (Ev ...))]
   )
 
 (define cek-red
@@ -28,12 +31,40 @@
    (--> (((o M N ...) ε) κ)
         ((M ε) (op (o) ((N ε) ...) κ))
         cek2)
-   (--> ((V ε_v) (fn ((λ X M) ε_x) κ))
-        ((M (update ε_x X (V ε_v))) κ)
+   (--> ((V ε) (fn ((λ X M) ε_x) κ))
+        ((M (update ε_x X (V ε))) κ)
         cek3
         (side-condition
-         (not (equal? (term V) (term X)))))
+         (not (redex-match cek-iswim X (term V)))))
+   (--> ((V ε) (ar (N ε_n) κ))
+        ((N ε_n) (fn (V ε) κ))
+        cek4
+        (side-condition
+         (not (redex-match cek-iswim X (term V)))))
+   (--> ((b_m ε) (op ((b_m-1 ε_m-1)... o) () κ))
+        (((δ ,(reverse (term (b_m b_m-1 ... o)))) ()) κ)
+        cek5)
+   (--> ((V ε_V) (op         (Cl_i ... o) ((N ε_N) Cl ...) κ))
+        ((N ε_N) (op ((V ε_V) Cl_i ... o)         (Cl ...) κ))
+        cek6
+        (side-condition
+         (not (redex-match cek-iswim X (term V)))))
    (--> ((X ε) κ)
-        ((lookup ε X) κ)
+        ((lookup X ε) κ)
         cek7)
    ))
+
+; ------------ Testing tools -------------
+
+(define (run-cek-test tm val)
+  (let [(res (apply-reduction-relation* cek-red
+                                        (term ((,tm ()) mt))))
+        (same? (lambda (res)
+                 (and
+                  (equal? (length res) 1)
+                  (equal? (caaar res) val))))]
+    
+    (test-predicate same? res)))
+
+(define (test-basics)
+  (run-basic-tests run-cek-test))
