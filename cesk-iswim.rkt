730a9ce1398afdd1d4d9fc4f03da2bcde94cd9c4
Ted Kirkpatrick <ted@sfu.ca>
2013-04-18 14:19:29 -0700

    Partway through adding let and seq to cesk-iswim

--- cesk-iswim.rkt
+++ cesk-iswim.rkt
@@ -9,7 +9,7 @@
 (define-extended-language cesk-iswim
   state-iswim
   (Cl (V ε)) ; Closure
-  (κ mt (fn Cl κ) (ar Cl κ) (op (V ... o) (Cl ...) κ) (set σ κ))
+  (κ mt (fn Cl κ) (ar Cl κ) (op (Cl ... o) ((M ε) ...) κ) (set σ κ))
   (Ev (X σ))
   (ε (Ev ...))
   (σ variable-not-otherwise-mentioned)
@@ -47,7 +47,7 @@
         ((M ε) S (op (o) ((N ε) ...) κ))
         cesk2)
    (--> ((V ε) S (fn ((λ X M) ε_f) κ))
-        ((M (update ε X σ_n)) S κ)
+        ((M (update ε_f X σ_n)) S κ)
         cesk3
         (side-condition
          (not (redex-match cesk-iswim X (term V))))
@@ -76,8 +76,55 @@
    (--> (Cl S (set σ κ))
         (,(update-and-return-prior! (term σ) (term Cl) Store) S x)
         cesk9)
+   ; Implement seq in reduction relation
+   (--> (((seq M) ε) S κ)
+        ((M ε) S κ)
+        seq-last)
+   (--> (((seq M M_i ...) ε) S κ)
+        ((((λ Y (seq M_i ...)) M) ε) S κ)
+        seq-n
+        (fresh Y)
+        (side-condition
+         (<= 1 (length (term (M_i ...))))))
+;   ; Implement let in reduction relation
+;   (--> ((in-hole E (let () in M)) S)
+;        ((in-hole E M) S)
+;        let-empty)
+;   (--> ((in-hole E (let ((X = M_v) (X_i = M_i) ...) in M)) S)
+;        ((in-hole E ((λ X (let ((X_i = M_i) ...) in M)) M_v)) S)
+;        let-n)
    ))
 
+; ------ Testing  module -----
+
+(module+ test
+  (require "basic-iswim-test.rkt")
+  (require "state-iswim-test.rkt")
+    
+  (define (run-test red tm val)
+    (let [(res (apply-reduction-relation* red
+                                          (term ((,tm ()) Uninit mt))))
+          (same? (lambda (res)
+                   (and
+                    (equal? (length res) 1)
+                    (equal? (caaar res) val))))]
+      
+      (test-predicate same? res)))
+  
+  (define (run-cesk-test tm val)
+    (run-test cesk-red tm val))
+  
+  (define (test-basics)
+    (run-basic-tests run-cesk-test))
+  
+  (define (test-state)
+    (run-state-tests run-cesk-test))
+  
+  (define (test-all)
+    (printf "basics: ") (test-basics)
+    ;(printf "state:  ") (test-state))
+  )
+  (test-all))
 
 
 
